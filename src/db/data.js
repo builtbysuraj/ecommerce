8e3609fe3077d2d14ffefc779d926b90f1eb1991
builtbysuraj <surajgupta3553@gmail.com>
2023-08-17 19:51:00 +0530

    chore: Remove comments

--- src/db/data.js
+++ src/db/data.js
@@ -342,19 +342,3 @@ export const data = [
     "thumbnail": "https://i.dummyjson.com/data/products/30/thumbnail.jpg"
   }
 ]
-
-// export const data = [
-//   {
-//     "id": 1,
-//     "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-//     "price": 109.95,
-//     "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-//     "category": "men's clothing",
-//     "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-//     "rating": {
-//       "rate": 3.9,
-//       "count": 120
-//     },
-//     "outOfStock": true,
-//     "fastDelivery": false
-//   },
